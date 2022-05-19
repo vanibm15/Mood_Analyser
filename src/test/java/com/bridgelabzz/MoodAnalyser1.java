f56207d1811d7bb53c1c76fe604c14b2d859e4c4
vanibm15 <vanibm15@gmail.com>
2022-05-19 19:53:16 +0530

    added:invalid mood

--- src/test/java/com/bridgelabzz/MoodAnalyser1.java
+++ src/test/java/com/bridgelabzz/MoodAnalyser1.java
@@ -3,19 +3,35 @@ package com.bridgelabzz;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyser1 {
-    String message;
+    private String message;
 
-
-    public static String analyzeMood(String message) {
-        try {
-            if (message.contains("Sad")) {
-                return "SAD";
-            }
-        } catch (NullPointerException e) {
-            return "HAPPY";
-        }
-        return null;
+    /*
+     * used default as well as parameterized constructor and defined the function
+     * which return the value as happy and sad.
+     */
+    public MoodAnalyser1() {
     }
+
+    public MoodAnalyser1(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+
+            if (this.message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (Exception e) {
+            if (message == null)
+                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.NULL);
+            else
+                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.EMPTY);
+        }
+    }
+}
+
 
 
 }
--- src/test/java/com/bridgelabzz/MoodAnalyser1Test.java
+++ src/test/java/com/bridgelabzz/MoodAnalyser1Test.java
@@ -1,33 +1,37 @@
 package com.bridgelabzz;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+class MoodAnayser1Test {
 
-class MoodAnalyser1Test extends MoodAnalyser1 {
-    public class TestMoodAnalyser {
 
-        public void givenMessage_SadMood_Should_Return_Sad() {
-            MoodAnalyser1 moodAnalyzer = new MoodAnalyser1("I am In a Sad Mood");
-            String mood = moodAnalyzer.analyseMood();
-            Assert.assertEquals("SAD", mood);
+    public void MoodAnalyserTest(String i_am_in_happy_mood) {
+        super(i_am_in_happy_mood);
+    }
+
+    @Test
+    public void givenMessage_SadMood_Should_Return_Sad() {
+        MoodAnalyser1 moodAnalyzer = new MoodAnalyser1("I am In a Sad Mood");
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            assertEquals("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
         }
-        @Test
-        public void givenMessage_SadMood_Should_Return_Happy() {
-            MoodAnalyser1 moodAnalyzer = new MoodAnalyser1("I am In a Sad Mood");
-            String mood = moodAnalyzer.analyseMood();
-            Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void givenMessage_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyser1 moodAnalyser = new MoodAnalyser1("I am in Happy Mood");
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
         }
-        @Test
-        public void givenMessage_AnyMood_Should_HandleException() {
-            MoodAnalyser1 moodAnalyser = new MoodAnalyser1(null);
-            String mood = moodAnalyser.analyseMood();
-            Assert.assertEquals("HAPPY", mood);
-           }
-      }
-
-
-  }
-
+    }
+}
 
